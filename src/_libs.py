3d3a62b6924ac61958a7bbdd9a27683df22c8ba1
hyugai <huyle210504@gmail.com>
2024-10-11 07:22:42 +0700

    m

---
+++ src/_libs.py
@@ -0,0 +1,26 @@
+# libs
+import re, os, sys, time
+
+# bs4
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+
+# selenium
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+# data structures / databases
+import numpy as np
+import pandas as pd
+import csv
+import sqlite3
+
+# type hint
+from typing import Iterator, Literal
+
+# Zillow
+from zillow_config import USER_AGENT
