c545bc8f2c9e39d27054cb3eb0e37210fdb727d2
huyle <huyle210504@gmail.com>
2024-10-11 23:25:32 +0700

    m

---
+++ tests/urlDetailInfo_exp.py
@@ -0,0 +1,28 @@
+# libs
+import os, sys
+cwd = os.getcwd()
+os.chdir('src/'); path_to_src = os.getcwd()
+os.chdir(cwd)
+if path_to_src not in sys.path:
+    sys.path.append(path_to_src)
+
+from _libs import *
+from _usr_libs import *
+
+# exp
+test_url = "https://www.zillow.com/homedetails/1611-Los-Alamos-Ave-SW-Albuquerque-NM-87104/6710669_zpid/"
+headers = {'User-Agent': USER_AGENT, 'Accept-Language': ACCEPT_LANGUAGE, 
+           'Accept-Encoding': ACCEPT_ENCODING}
+with requests.Session() as s:
+    r = s.get(test_url, headers=headers)
+    if r.status_code != 200:
+        print(r.status_code)
+    else:
+        soup = BeautifulSoup(r.content.decode("utf-8"), features="lxml")
+        text = re.sub(r'&amp;', '&', r.content.decode("utf-8"))
+        text_soup = BeautifulSoup(text, features="lxml")
+        dom = etree.HTML(str(soup))
+
+    # test
+    print(text_soup.prettify())
+    ## test
