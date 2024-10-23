0e349ce9672be7c68fb3ef3425fb1c0e777060f4
hyugai <huyle210504@gmail.com>
2024-10-23 09:00:04 +0700

    m

--- test.py
+++ test.py
@@ -11,11 +11,20 @@ from _usr_libs import *
 # exp
 #proxies = {'http': 'http://142.171.102.136:3128', 
 #           'https': 'http://117.54.114.99:80'}
-#headers = {'User-Agent': random.choice(USER_AGENTS), 'Accept-Encoding': ACCEPT_ENCODING, 
-#           'Accept-Language': ACCEPT_LANGUAGE}
+headers = {'User-Agent': random.choice(USER_AGENTS), 'Accept-Encoding': ACCEPT_ENCODING, 
+           'Accept-Language': ACCEPT_LANGUAGE}
 #with requests.Session() as s:
 #    r = s.get(HOMEPAGE_URL, headers=headers, proxies=proxies)
 #    if r.status_code != 200:
 #        print(r.status_code)
 #    else:
 #        print(r.text)
+with requests.Session() as s:
+#    s.proxies = {}
+#    s.proxies['http'] = 'socks5h://localhost:9050'
+#    s.proxies['https'] = 'socks5h://localhost:9050'
+    r = s.get(HOMEPAGE_URL, headers=headers)
+    if r.status_code != 200:
+        print(r.text)
+    else:
+        print(r.text)
