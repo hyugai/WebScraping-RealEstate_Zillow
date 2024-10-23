fb2cb6463e9d60238f8a4b129def056dc0a95305
hyugai <huyle210504@gmail.com>
2024-10-23 08:10:59 +0700

    m

--- test.py
+++ test.py
@@ -9,5 +9,13 @@ from _libs import *
 from _usr_libs import *
 
 # exp
-with sqlite3.connect('tests/dbs/real_estate.db') as conn:
-    pd.read_sql("select * from city_url", conn).to_csv('test.csv', index=False)
+#proxies = {'http': 'http://142.171.102.136:3128', 
+#           'https': 'http://117.54.114.99:80'}
+#headers = {'User-Agent': random.choice(USER_AGENTS), 'Accept-Encoding': ACCEPT_ENCODING, 
+#           'Accept-Language': ACCEPT_LANGUAGE}
+#with requests.Session() as s:
+#    r = s.get(HOMEPAGE_URL, headers=headers, proxies=proxies)
+#    if r.status_code != 200:
+#        print(r.status_code)
+#    else:
+#        print(r.text)
