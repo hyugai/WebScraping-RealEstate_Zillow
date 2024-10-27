4d970b371e4882681bde5ab06ab37bd30b63ed78
huyle <huyle210504@gmail.com>
2024-10-27 13:04:39 +0700

    m

--- src/draft.py
+++ src/draft.py
@@ -12,8 +12,25 @@ from _usr_libs import *
 class GeneralScrapper(TableTracker):
     def __init__(self, 
                  db_path: str, table_name: str, 
-                 headers: dict, cities_urls: list[str]
-) -> None:
+                 headers: dict, cities_urls: list[str]) -> None:
         super().__init__(db_path, table_name)
         self.headers = headers
         self.cities_urls = cities_urls
+        
+    async def func_01(self, 
+                      s: aiohttp.ClientSession, url: str):
+        async with s.get(url) as r:
+            pass 
+
+    async def test(self):
+        async with aiohttp.ClientSession(headers=self.headers) as s:
+            pass
+
+    def extract(self):
+        pass
+
+    def transform(self):
+        pass
+
+    def load(self):
+        pass
