78c08fbc0b71f597a1be124b0192d94edc1cb079
huyle <huyle210504@gmail.com>
2024-10-27 09:49:39 +0700

    m

--- tests/concurrency/async.py
+++ tests/concurrency/async.py
@@ -11,12 +11,21 @@ from _usr_libs import *
 # exp
 urls = [HOMEPAGE_URL]*5
 
-async def fetch(session: aiohttp.ClientSession, url: str):
+async def fetch(
+    session: aiohttp.ClientSession, url: str
+) -> None:
     async with session.get(url) as r:
-        print(f"Status: {r.status}")
-        await r.text
+        print(r.status)
+        await r.text()
 
-async def main():
-   #queue = asyncio.Queue() 
-   async with aiohttp.ClientSession() as session:
-       pass
+async def main() -> None:
+    headers = {'Accept-Language': ACCEPT_LANGUAGE, 'Accept-Encoding': ACCEPT_ENCODING, 
+                'User-Agent': UserAgent().random}
+    async with aiohttp.ClientSession(headers=headers) as s:
+        tasks = []
+        for url in urls:
+            tasks.append(fetch(s, url))
+        
+        await asyncio.gather(*tasks)
+
+asyncio.run(main())
