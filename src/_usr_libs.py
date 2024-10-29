c03927019d988fb9ce75f12e67f0b198cfc79e67
hyugai <huyle210504@gmail.com>
2024-10-29 09:22:47 +0700

    m

--- src/_usr_libs.py
+++
@@ -1,5 +0,0 @@
-# Zillow
-from zillow_config import *
-from trackers import TableTracker, JSONTracker, IPTracker
-from etl_pipelines import CityURLScrapper, GeneralHomeScrapper_RE
-from scrappers import GeneralInfoScrapper
--- src/zillow_config.py
+++
@@ -1,3 +0,0 @@
-ACCEPT_LANGUAGE = "en-US,en;q=0.9,vi;q=0.8,nl;q=0.7"
-ACCEPT_ENCODING = "gzip, deflate, br, zstd, sdch"
-HOMEPAGE_URL = "https://www.zillow.com"
