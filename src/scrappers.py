088d8ba430522b1e220aa3416fbef954c20f2079
hyugai <huyle210504@gmail.com>
2024-10-29 12:43:45 +0700

    m

--- src/scrappers.py
+++ src/scrappers.py
@@ -9,7 +9,7 @@ class URLScrapper(TableTracker):
         super().__init__(path, name)
         self.headers = headers
 
-    def cities_collector(self) -> str:
+    def cities_collector(self) -> list[str]:
         with requests.Session() as s:
             self.headers['User-Agent'] = UserAgent().random 
             r = s.get(ZILLOW, headers=self.headers) 
@@ -19,11 +19,29 @@ class URLScrapper(TableTracker):
                 xpath = "//button[text()='Real Estate']/parent::div/following-sibling::ul/child::li/descendant::a"
                 nodes_a = dom.xpath(xpath)
                 full_hrefs = [ZILLOW + node.get('href') for node in nodes_a]
+
+                return full_hrefs
             else:
                 raise ValueError(f'Failed fetching (error code: {r.status_code})')
 
-    def pages_collector(self):
-        pass
+    async def pages_collector(self,
+                                s: aiohttp.ClientSession, href: str, 
+                                queue: asyncio.Queue):
+        async with s.get(href) as r:
+            if r.status == 200:
+                content = await r.text()
+            else:
+                print(r.status)
 
-    def main_extract(self):
-        pass
+    async def main_extract(self):
+        hrefs = self.cities_collector()
+
+        self.headers['User-Agent'] = UserAgent().random
+        queue = asyncio.Queue()
+        async with aiohttp.ClientSession(headers=self.headers) as s:
+            tasks = [self.pages_collector(s, href, queue) for href in hrefs] 
+            
+            await asyncio.gather(*tasks)
+    
+    def main(self):
+        asyncio.run(self.main_extract())
