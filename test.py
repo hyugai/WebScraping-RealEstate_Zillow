5513cf7dc6505abd41d185ebdcc4bbd821ac5c2a
hyugai <huyle210504@gmail.com>
2024-11-06 11:09:59 +0700

    m

--- test.py
+++ test.py
@@ -1,36 +1,38 @@
 import sys
 from pathlib import Path
+
+import aiohttp.client_exceptions
 path_to_src = (Path.cwd()/'src').as_posix()
 if path_to_src not in sys.path:
     sys.path.append(path_to_src) 
 from libs import * 
 
-# exp
-#scrape_freeProxyList()
-#csv_path = (Path.cwd()/'tests'/'resource'/'proxies'/'free_proxy_list.csv').as_posix()
-#df_proxies = pd.read_csv(csv_path)
-#proxies_pool = [f"http://{ip}:{port}" for ip, port in zip(df_proxies['ip_address'], df_proxies['port'])]    
-#
-#async def fetch_url():
-#    async with aiohttp.ClientSession(headers={'Referer': 'https://www.google.com.vn'}) as s:
-#        while True:
-#            try:
-#                headers = ZILLOW_HEADERS
-#                headers['User-Agent'] = UserAgent().random
-#                async with s.get('https://www.zillow.com/albuquerque-nm/', headers=headers, proxy=random.choice(proxies_pool)) as r:
-#                    if r.status == 200:
-#                        content = await r.text()
-#                        print(BeautifulSoup(content, features='lxml').prettify())
-#
-#                        break
-#                    else:
-#                        continue
-#            except Exception:
-#                continue 
-#
-#asyncio.run(fetch_url())
+#exp
+scrape_freeProxyList()
+csv_path = (Path.cwd()/'tests'/'resource'/'proxies'/'free_proxy_list.csv').as_posix()
+df_proxies = pd.read_csv(csv_path)
+proxies_pool = [f"http://{ip}:{port}" for ip, port in zip(df_proxies['ip_address'], df_proxies['port'])]    
 
-with open('test.txt', 'r') as f:
-    text = f.read()
+async def fetch_url():
+    async with aiohttp.ClientSession(headers={'Referer': 'https://www.google.com.vn'}, connector=aiohttp.TCPConnector()) as s:
+        while True:
+            try:
+                headers = ZILLOW_HEADERS
+                headers['User-Agent'] = UserAgent().random
+                proxy = random.choice(proxies_pool)
+                async with s.get('https://www.zillow.com/albuquerque-nm/', headers=headers, proxy=proxy) as r:
+                    if r.status == 200:
+                        print('OK')
 
-text = json.loads(text)
+                        break
+                    else:
+                        continue
+            except Exception as e:
+                print(e, proxy)
+
+asyncio.run(fetch_url())
+
+#with open('test.txt', 'r') as f:
+#    text = f.read()
+#
+#text = json.loads(text)
