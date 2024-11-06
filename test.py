5f00091280fb2ac6a64c61357823b115487b1475
hyugai <huyle210504@gmail.com>
2024-11-06 13:23:43 +0700

    m

--- test.py
+++ test.py
@@ -8,31 +8,43 @@ if path_to_src not in sys.path:
 from libs import * 
 
 #exp
-scrape_freeProxyList()
-csv_path = (Path.cwd()/'tests'/'resource'/'proxies'/'free_proxy_list.csv').as_posix()
-df_proxies = pd.read_csv(csv_path)
-proxies_pool = [f"http://{ip}:{port}" for ip, port in zip(df_proxies['ip_address'], df_proxies['port'])]    
+def scrap_1Page():
+    scrape_freeProxyList()
+    csv_path = (Path.cwd()/'tests'/'resource'/'proxies'/'free_proxy_list.csv').as_posix()
+    df_proxies = pd.read_csv(csv_path)
+    proxies_pool = [f"http://{ip}:{port}" for ip, port in zip(df_proxies['ip_address'], df_proxies['port'])]    
 
-async def fetch_url():
-    async with aiohttp.ClientSession(headers={'Referer': 'https://www.google.com.vn'}, connector=aiohttp.TCPConnector()) as s:
-        while True:
-            try:
-                headers = ZILLOW_HEADERS
-                headers['User-Agent'] = UserAgent().random
-                proxy = random.choice(proxies_pool)
-                async with s.get('https://www.zillow.com/albuquerque-nm/', headers=headers, proxy=proxy) as r:
-                    if r.status == 200:
-                        print('OK')
+    async def fetch_url():
+        async with aiohttp.ClientSession(headers={'Referer': 'https://www.google.com.vn'}, connector=aiohttp.TCPConnector()) as s:
+            while True:
+                try:
+                    headers = ZILLOW_HEADERS
+                    headers['User-Agent'] = UserAgent().random
+                    proxy = random.choice(proxies_pool)
+                    async with s.get('https://www.zillow.com/albuquerque-nm/', headers=headers, proxy=proxy) as r:
+                        if r.status == 200:
+                            print('OK')
 
-                        break
-                    else:
-                        continue
-            except Exception as e:
-                print(e, proxy)
+                            break
+                        else:
+                            continue
+                except Exception as e:
+                    print(e, proxy)
 
-asyncio.run(fetch_url())
+    asyncio.run(fetch_url())
 
-#with open('test.txt', 'r') as f:
-#    text = f.read()
-#
-#text = json.loads(text)
+def handle_string():
+    with open('test.txt', 'r') as f:
+        text = f.read()
+
+    text: dict = json.loads(text)
+    key_to_find = 'listResults'
+    while key_to_find not in text:
+        tmp_dict = {}
+        [tmp_dict.update(value) for value in text.values() if isinstance(value, dict)]
+        text = tmp_dict 
+
+    homes_info = text[key_to_find]
+    pd.json_normalize(homes_info).to_csv('fee.csv', index=False)
+    
+handle_string()
